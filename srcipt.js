7b35a875ddf5c22dd60c770a24b5bd5f9becc80b
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-22 22:02:24 +0530

    data type

--- srcipt.js
+++ srcipt.js
@@ -1,11 +1,24 @@
-// // var let const 
-// // declaration and initailization 
-// // schope (globle ,block ,function)
-// let a=10
-// {
-//     let a=12
-//     console.log("inner..",a)
-// }
-// console.log("outer..",a)
- let a = [1,2,3,4];
- let b = a;
+// // // // var let const 
+// // // // declaration and initailization 
+// // // // schope (globle ,block ,function)
+// // // let a=10
+// // // {
+// // //     let a=12
+// // //     console.log("inner..",a)
+// // // }
+// // // console.log("outer..",a)
+//  let a = [1,2,3,4];
+//  let b = a;
+// b.pop()
+
+// ............AbortController......AbortController.apply.......
+
+
+// chatper 2>>>>>>>>>>>>>>
+
+// string , number , boolen , null , undefined , symbol ,bigint 
+// array ,obejcts, functions 
+
+// ''  >> single queotes
+// ""  >> dobule quoets 
+// ``  >> backticks 
