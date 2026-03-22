76a050a7a6a20f11c0dfe446c36923e69c5ddd77
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-22 21:54:33 +0530

    array

--- srcipt.js
+++ srcipt.js
@@ -1,9 +1,11 @@
-// var let const 
-// declaration and initailization 
-// schope (globle ,block ,function)
-let a=10
-{
-    let a=12
-    console.log("inner..",a)
-}
-console.log("outer..",a)
+// // var let const 
+// // declaration and initailization 
+// // schope (globle ,block ,function)
+// let a=10
+// {
+//     let a=12
+//     console.log("inner..",a)
+// }
+// console.log("outer..",a)
+ let a = [1,2,3,4];
+ let b = a;
