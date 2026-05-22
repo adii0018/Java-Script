6ff57ea85c2ab11db40c98a9eea609ab2bc6ff45
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-22 17:13:18 +0530

    19

--- 01/p8.js
+++ 01/p8.js
@@ -1,6 +1,9 @@
+//Example 1: Simple Symbol
 {
-    let id1=symobol("id")
-    let id2=symobol("id")
-    console.log(id1===id2)
-    
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+
+console.log(id1 === id2); // ❌ false (unique hote hain)
+
 }
+//yahan dono ka description "id" same hai, par values alag aur unique hain.
