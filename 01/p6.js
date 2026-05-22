6134a04b36f282d6db30f5cdc4ab5d00b9102947
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-22 16:48:01 +0530

    16

---
+++ 01/p6.js
@@ -0,0 +1,10 @@
+//Null → intentional empty value
+
+{
+    let a=null;
+    if(a===null){
+        console.log("yeeh abhi null he yrr");
+    } else{
+        console.log("aab null nhi he yehh he value uski >>"+null)
+    }
+}
