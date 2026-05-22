213c08de497c7d357c2905b280ca166641052fbd
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-22 16:24:50 +0530

    13

---
+++ 01/p3.js
@@ -0,0 +1,5 @@
+//Bhai, JavaScript me user se input lena directly possible nahi hota jaise C ya Java me scanf / Scanner hota hai. Lekin kuch tareeke hote hain jo environment pe depend karte hain
+{
+let name = prompt("Apna naam likho:");
+console.log("Namaste " + name);
+}
