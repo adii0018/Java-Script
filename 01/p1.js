ac74fa84dd1a0d99247c70074cd91859d8cd838b
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-22 16:11:07 +0530

    11

---
+++ 01/p1.js
@@ -0,0 +1,6 @@
+//JavaScript me primitive data types wo hote hain jo basic aur fundamental values ko represent karte hain. Ye immutable hote hain (yaani ek baar banne ke baad unki value directly change nahi hoti, balki nayi value assign karni padti hai).
+
+//1 String → text ko represent karta hai
+
+var name = "adii"
+console.log(name)
