dcb56f9b41fb84a85a6c31dbdb3f9428b025fceb
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-23 14:08:41 +0530

    38

---
+++ Object Data Types/f5.js
@@ -0,0 +1,11 @@
+// JavaScript mein function bhi ek object hai. Matlab tum function ke andar bhi properties daal sakte ho:
+
+{
+  function hey(){
+    return "jay shree ram";
+  }
+  hey.n1="yeeeeeeeehhhhhhhhh"
+  console.log(hey())
+  console.log(hey.n1)  
+
+}
