8cbb95aa520e0fd3418e535d38f3020fd4dc54c2
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-23 12:02:09 +0530

    27

---
+++ Object Data Types/a2.js
@@ -0,0 +1,11 @@
+// 1: Fruits Array
+
+{
+    const fruts=["apple","mango","bannana","puppaya","graps"];
+    console.log(fruts[0])
+    console.log(fruts[1])
+    console.log(fruts[2])
+    console.log(fruts[3])
+    console.log(fruts[4])
+
+}
---
+++ Object Data Types/a3.js
@@ -0,0 +1,7 @@
+// 2: Numbers Array + Loop
+
+{
+    let number=[39,32,23,55];
+    for (let i =0; i<number.length; i++)
+        console.log("Index  "   +i+  " vale:  "+number[i])
+}
