10c4ad0bd2c4dcd5bb26b567ea96cd4892205ddd
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-23 14:35:36 +0530

    40

---
+++ Object Data Types/f7.js
@@ -0,0 +1,12 @@
+// 4: Function Stored in Variable
+
+{
+    let yeeh = function(name){
+        console.log("hey  "+name+" rajput")
+    }
+    yeeh("adii")
+
+
+// Function ko variable me store karke bhi use kar sakte ho.
+
+}
