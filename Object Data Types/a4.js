6f7a4dec4366e7bdede029479293dcf43b6b972a
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-23 12:15:59 +0530

    28

---
+++ Object Data Types/a4.js
@@ -0,0 +1,19 @@
+// 3: Array Methods
+
+{
+let colors = ["Red", "Green", "Blue"];
+
+colors.push("Yellow");   // add at end
+console.log(colors);     // ✅ ["Red","Green","Blue","Yellow"]
+
+colors.pop();            // remove last
+console.log(colors);     // ✅ ["Red","Green","Blue"]
+
+colors.shift();          // remove first
+console.log(colors);     // ✅ ["Green","Blue"]
+
+colors.unshift("Black"); // add at start
+console.log(colors);     // ✅ ["Black","Green","Blue"]
+
+
+}
---
+++ Object Data Types/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+    // color.push("red");
+    // console.log(color)
+    
+    // color.pop();
+    // console.log(color)
+
+    // color.shift();
+    // console.log(color)
