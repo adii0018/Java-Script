f533ec240abc0e4247c26ac078c1399578f54634
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-23 11:29:06 +0530

    25

---
+++ Object Data Types/o4.js
@@ -0,0 +1,12 @@
+// 4: Dynamic Property Add/Remove
+
+{
+    let person={
+        name:"adii"
+    };
+    person.surname="Singh rajput"
+    console.log(person)
+
+    delete person.name;
+    console.log(person)
+}
