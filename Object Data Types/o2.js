157d70deacf26bbe7ba51d4ff7729de74d7688c1
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-23 11:15:47 +0530

    24

---
+++ Object Data Types/o2.js
@@ -0,0 +1,12 @@
+//2: Nested Object
+
+{
+    let cricket={
+        country:"india",
+        jno:{
+            name:"virat kohli",
+            Team:"RCB"
+        }
+    }
+    console.log(cricket.jno.Team)
+}
---
+++ Object Data Types/o3.js
@@ -0,0 +1,10 @@
+// 3: Object with Function
+{
+   let person ={
+    name :"adii",
+    neww:function(){
+        console.log("hello," +this.name,"🌸")
+    }
+   };
+   person.neww()
+}
