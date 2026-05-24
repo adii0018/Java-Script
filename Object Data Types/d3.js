d1c02fde3755f896a369d84c40ecbdffd786d7bf
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-24 18:45:44 +0530

    46

---
+++ Object Data Types/d3.js
@@ -0,0 +1,18 @@
+// 3: Date Methods
+
+{
+    let a= new Date();
+
+    console.log(a.getFullYear())
+    console.log(a.getDate())
+    console.log(a.getMonth())
+    console.log(a.getDay())
+    console.log(a.getTime())
+    console.log(a.getHours())
+    console.log(a.getMinutes())
+    console.log(a.getMilliseconds())
+    console.log(a.getTimezoneOffset())
+    
+
+
+}
