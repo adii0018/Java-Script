75ee75b6a6017b04c38621d4c4ff3d906a71a9ad
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-24 19:05:14 +0530

    51

--- Object Data Types/d5.js
+++ Object Data Types/d5.js
@@ -5,6 +5,6 @@
     let a= new Date()
     a.setDate(a.getDate()+7)
     console.log(a)
-
     
+
 }
---
+++ Object Data Types/r1.js
@@ -0,0 +1,10 @@
+// 1: Simple Pattern MatchA
+
+{
+
+    let a = /hello/;
+    console.log(a.test("hello world"))
+    console.log(a.test("hell"))
+    
+}
+
