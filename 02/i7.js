7aba41d6f8660b42716a131e694149c27f315abf
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-25 20:33:21 +0530

    61

---
+++ 02/i7.js
@@ -0,0 +1,12 @@
+// convert a num into string
+{
+   let a = 89;
+   console.log(typeof String(a))
+   console.log(typeof (a + ""));
+
+}
+// {
+//     let a =77;
+//     let b=string(a);
+//     console.log(b)
+// }
