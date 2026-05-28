db7088e8cd8b52e20ce86f368a78d5f490eb3746
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-28 22:54:46 +0530

    67

--- 02/i10.js
+++ 02/i10.js
@@ -1,4 +1,11 @@
-{
-    let a = "adii"
-    console.log(a);
-}
+// Basic JavaScript Example
+
+// Do numbers define karte hain
+let a = 10;
+let b = 20;
+
+// Addition karte hain
+let sum = a + b;
+
+// Console me output dikhate hain
+console.log("Sum = " + sum);
