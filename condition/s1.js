fe18a9fa686e1de76970f6310cfe5fb9e7a85dab
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-06-05 10:35:22 +0530

    8

---
+++ condition/s1.js
@@ -0,0 +1,19 @@
+var team = "rcsxsab" ;
+
+switch(team){
+    case("rcb"):
+    console.log("Ee sala cup namdu")
+    break;
+
+    case("mi"):
+    console.log("duniya hila denege")
+    break;
+
+    case("csk"):
+    console.log("wisele poduuuu")
+    break;
+
+    default:
+        console.log("loser teams choise ")
+        break;
+}
