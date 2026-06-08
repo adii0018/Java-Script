df7710dd46e1186b40a55c60870fa1bb9642c760
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-06-09 04:12:03 +0530

    34

---
+++ Function/08.js
@@ -0,0 +1,8 @@
+const isreverse =(str)=>{
+    for(let char =str.length -1; char >=0;char--){
+        console.log(str[char])
+    }
+      
+}
+
+isreverse("adii rajput ")
---
+++ Function/09.js
@@ -0,0 +1,8 @@
+const isreverse =(str)=>{
+    for(let char =0; char <=str.length-1;char++){
+        console.log(str[char])
+    }
+      
+}
+
+isreverse("adii rajput")
