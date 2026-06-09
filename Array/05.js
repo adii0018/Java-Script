69cbeb3f13f024c2ead554b315acd128cfb116e6
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-06-09 23:54:13 +0530

    39

---
+++ Array/05.js
@@ -0,0 +1,19 @@
+{
+    let rcb =[
+  "Faf du Plessis",
+  "Virat Kohli",
+  "Glenn Maxwell",
+  "Dinesh Karthik",
+  "Rajat Patidar",
+  "Mohammed Siraj",
+  "Cameron Green",
+  "Will Jacks",
+  "Mahipal Lomror",
+  "Karn Sharma",
+  "Akash Deep"
+];
+
+for(let asr of rcb)
+    console.log(asr)
+
+}
