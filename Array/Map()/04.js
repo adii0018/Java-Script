3c7bfcd56939e919850982e3f161118353ac2ed8
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-06-13 11:33:46 +0530

    12

---
+++ Array/Map()/04.js
@@ -0,0 +1,13 @@
+// combination of filter  and map()
+
+let students = [
+  {name: "Amit", marks: 80},
+  {name: "Ravi", marks: 45},
+  {name: "Neha", marks: 90}
+];
+
+
+let passnames = students.filter(s=>s.marks > 50).map(s=>s.name)
+
+console.log(passnames)
+
