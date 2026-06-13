fd06c92d5171fece3c038dcf4f4f69e1b87380d2
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-06-13 11:26:48 +0530

    10

---
+++ Array/Map()/01.js
@@ -0,0 +1,6 @@
+//Numbers ko double karna
+
+let a = [1,2,4,5,6,7,8,9]
+let b = a.map(a=>a/2)
+
+console.log(b)
---
+++ Array/Map()/02.js
@@ -0,0 +1,6 @@
+//Strings ko uppercase me convert karna
+
+let a =["asklc sakc as,m ca cksncan"]
+
+let b = a.map(a=>a.toUpperCase())
+console.log(b)
